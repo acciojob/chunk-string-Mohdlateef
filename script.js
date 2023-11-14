c64484d1f4f4130869fc824b4ec9a5fe0071ab80
LATEEF AHMAD BABA <91480408+Mohdlateef@users.noreply.github.com>
2023-11-14 17:59:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 function stringChop(str, size) {
 	
+	
   // your code here
-	let i=0, ans={};
+	if(str===null)
+	{
+		return [];
+	}
+	let i=0, ans=[];
 	while(i<str.length-1)
 		{
 			displayError:"TypeError: The following error originated from your application code, not from Cypress. > Cannot read properties of null (reading 'length') When Cypress detects uncaught errors originating from your application it will automatically fail the current test. This behavior is configurable, and you can choose to turn this off by listening to the `uncaught:exception` event. https://on.cypress.io/uncaught-exception-from-application at stringChop (http://localhost:3000/script.js:4:15) at http://localhost:3000/script.js:16:7"
@@ -13,6 +18,6 @@ function stringChop(str, size) {
 }
 
 // Do not change the code below
-const str = prompt("Enter String.");
-const size = prompt("Enter Chunk Size.");
-alert(stringChop(str, size));
+// const str = prompt("Enter String.");
+// const size = prompt("Enter Chunk Size.");
+// alert(stringChop(str, size));
